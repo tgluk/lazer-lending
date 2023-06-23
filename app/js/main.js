a4aff90efe960284effbdb742007057ec49b309c
tgluk <timi.biz@bk.ru>
2023-06-23 12:01:15 +0400

    fix: production logic

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 // slider
 
+// const { get } = require("browser-sync")
+
 let screenWidth // переменная для расстояния между слайдами в зависимости от размера экрана
 
-function mediaQuery() {
+const mediaQuery = () => {
   if (window.matchMedia('(min-width: 1025px)').matches ) {
     screenWidth = 40
   }
@@ -18,7 +20,7 @@ function mediaQuery() {
 mediaQuery()
 
 const swiper = new Swiper('.swiper', {
-  // цикличность слайдераа
+  // цикличность слайдера
   loop: true,
   // включение стрелок
   navigation: {
@@ -37,4 +39,45 @@ const swiper = new Swiper('.swiper', {
   spaceBetween: screenWidth, // расстояние между слайдами
   // курсор - рука
   grabCursor: true,
+})
+
+// tabs
+document.addEventListener('DOMContentLoaded', () => {
+const tabs = () => {
+  const head = document.querySelector('.tabs__head')
+  const body = document.querySelector('.tabs__body')
+
+  const getActiveTabName = () => {
+    return head.querySelector('.tabs__title--active').dataset.tab
+  }
+
+  const setActiveContent = () => {
+    if (body.querySelector('.tabs__content--active')) {
+      body.querySelector('.tabs__content--active').classList.remove('tabs__content--active')
+    }
+    body.querySelector(`[data-tab=${getActiveTabName()}]`).classList.add('tabs__content--active')
+  }
+
+  if (!head.querySelector('.tabs__title--active')) {
+    head.querySelector('.tabs__title').classList.add('.tabs__title--active')
+  }
+
+  setActiveContent(getActiveTabName())
+
+  head.addEventListener('click', e => {
+    const caption = e.target.closest('.tabs__title')
+    if (!caption) return
+    if (caption.classList.contains('tabs__title--active')) return
+
+    if (head.querySelector('.tabs__title--active')) {
+      head.querySelector('.tabs__title--active').classList.remove('tabs__title--active')
+    }
+
+    caption.classList.add('tabs__title--active')
+
+    setActiveContent(getActiveTabName())
+  })
+}
+
+tabs()
 })
