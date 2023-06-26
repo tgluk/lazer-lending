b95791eb1f242951fa5dc27d3c2c3c891149211f
timur-webdev <timi.biz@bk.ru>
2023-06-26 09:10:11 +0400

    fix: tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,6 @@
 // slider
 
-// const { get } = require("browser-sync")
-
 let screenWidth // переменная для расстояния между слайдами в зависимости от размера экрана
-
 const mediaQuery = () => {
   if (window.matchMedia('(min-width: 1025px)').matches ) {
     screenWidth = 40
@@ -43,41 +40,71 @@ const swiper = new Swiper('.swiper', {
 
 // tabs
 document.addEventListener('DOMContentLoaded', () => {
-const tabs = () => {
-  const head = document.querySelector('.tabs__head')
-  const body = document.querySelector('.tabs__body')
+  const tabletWindow = window.matchMedia("(max-width: 840px)").matches
+  const tabs = () => {
+    const head = document.querySelector('.tabs__head')
+    const body = document.querySelector('.tabs__body')
 
-  const getActiveTabName = () => {
-    return head.querySelector('.tabs__title--active').dataset.tab
-  }
-
-  const setActiveContent = () => {
-    if (body.querySelector('.tabs__content--active')) {
-      body.querySelector('.tabs__content--active').classList.remove('tabs__content--active')
-    }
-    body.querySelector(`[data-tab=${getActiveTabName()}]`).classList.add('tabs__content--active')
-  }
-
-  if (!head.querySelector('.tabs__title--active')) {
-    head.querySelector('.tabs__title').classList.add('.tabs__title--active')
-  }
-
-  setActiveContent(getActiveTabName())
-
-  head.addEventListener('click', e => {
-    const caption = e.target.closest('.tabs__title')
-    if (!caption) return
-    if (caption.classList.contains('tabs__title--active')) return
-
-    if (head.querySelector('.tabs__title--active')) {
-      head.querySelector('.tabs__title--active').classList.remove('tabs__title--active')
+    const getActiveTabName = () => {
+      return head.querySelector('.tabs__title--active').dataset.tab
     }
 
-    caption.classList.add('tabs__title--active')
+    const setActiveContent = () => {
+      if (body.querySelector('.tabs__content--active')) {
+        body.querySelector('.tabs__content--active').classList.remove('tabs__content--active')
+      }
+      body.querySelector(`[data-tab=${getActiveTabName()}]`).classList.add('tabs__content--active')
+    }
+
+    if (!head.querySelector('.tabs__title--active')) {
+      head.querySelector('.tabs__title').classList.add('.tabs__title--active')
+    }
 
     setActiveContent(getActiveTabName())
-  })
-}
 
-tabs()
-})
+    head.addEventListener('click', e => {
+      const caption = e.target.closest('.tabs__title')
+      // if (!caption) return
+      // if (caption.classList.contains('tabs__title--active')) return
+
+      if (head.querySelector('.tabs__title--active')) {
+        head.querySelector('.tabs__title--active').classList.remove('tabs__title--active')
+      }
+
+      caption.classList.add('tabs__title--active')
+
+      setActiveContent(getActiveTabName())
+      // console.log(true)
+
+      // if (tabletWindow) {
+      //   // console.log(true)
+      //   let headTitles = [...document.querySelectorAll('.tabs__title')]
+      //   headTitles.map(title => {
+      //     // console.log(true)
+      //     title.style.display = 'block'
+      //     // title.addEventListener('click', e => {
+      //     //   title.style.display = 'none'
+      //     // })
+      //   })
+      // }
+    })
+  }
+
+  tabs()
+
+  if (tabletWindow) {
+    const head = document.querySelector('.tabs__head')
+    head.addEventListener('click', e => {
+      let headTitles = [...document.querySelectorAll('.tabs__title')]
+      headTitles.map(title => {
+        if (window.getComputedStyle(title,null).getPropertyValue("display") == 'block') {
+          title.style.display = 'none'
+        } else {
+          title.style.display = 'block'
+        }
+        // console.log(window.getComputedStyle(title,null).getPropertyValue("display"))
+      })
+      console.log(true)
+    })
+  }
+}) 
